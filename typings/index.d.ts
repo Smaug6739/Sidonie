0c9d04890f5d36bb7f516e484d6a5a90b0b31d4a
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-25 18:13:24 +0200

    Update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,20 +1,33 @@
 /// <reference types="node" />
-import { EventEmitter } from "events";
+import { EventEmitter } from "node:events";
 import type { JsonData } from "./types";
-export default class Sidonie extends EventEmitter {
+export declare class Sidonie extends EventEmitter {
     private path;
     private cache;
     private needFetch;
     private isWriting;
     constructor(path: string);
+    /**
+     * > The function `init` will initialize database.
+     */
     init(): Promise<void>;
+    /**
+     * It reads the file, parses it, and returns the parsed data
+     * @returns The data from the file.
+     */
     fetch(): Promise<JsonData | null>;
+    /**
+     * If the fetch flag is true and the needFetch flag is true, then fetch the data and set the cache to
+     * the result
+     * @param {boolean} [fetch=false] - If true, fetch the data.
+     * @returns The cache
+     */
     getAll(fetch?: boolean): Promise<JsonData>;
     /**
      * If fetch is true, and we need to fetch, then fetch, and if the result is truthy, then set the cache
      * to the result, and set needFetch to false.
      * @param {string | number} item - The item to get from the cache.
-     * @param {boolean} [fetch=false] - boolean = false
+     * @param {boolean} [fetch=false] - If true, fetch the data.
      * @returns The value of the item in the cache.
      */
     get(item: string | number, fetch?: boolean): Promise<any>;
